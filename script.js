535b4393472cc6ffe85bf305a943b70b22561bb2
Mariana Maia <marianalima.amb@gmail.com>
2022-12-07 14:14:25 -0300

    done

--- script.js
+++ script.js
@@ -2,42 +2,35 @@ const nameInput = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
-const submitBtn = document.querySelector("#claimBtn");
+
 const form = document.querySelector("#form_section");
-const errorName = document.querySelector("#error__name");
-const errorLastname = document.querySelector("#error__lastname");
-const errorEmail = document.querySelector("#error__email");
-const errorPassword = document.querySelector("#error__password");
+const errorName = document.querySelector(".error__name");
+const errorLastname = document.querySelector(".error__lastname");
+const errorEmail = document.querySelector(".error__email");
+const errorPassword = document.querySelector(".error__password");
 
-const isEmailValid = (email) => {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-};
+// console.log((errorName.textContent = "nome errado"));
 
-function handleBlank() {
-  if (nameInput.value.length === 0) {
-    errorName.textContent = " Cannot be blank!";
-  } else if (lastName.value.length === 0) {
-    errorLastname.textContent = " Cannot be blank!";
-  } else if (email.value.length === 0) {
-    errorEmail.textContent = " Cannot be blank!";
-  } else if (password.value.length === 0) {
-    errorPassword.textContent = " Cannot be blank!";
-  }
-  return;
-}
-
-nameInput.onkeyup = function () {
-  let nameform = nameInput.value;
-  if (nameform.length === 0) {
-    errorName.textContent = "testando";
-  } else {
-    errorName.textContent = "";
-  }
-};
-
-form.addEventListener("submit", function (event) {
+function handleConfirmBtn(event) {
   event.preventDefault();
-  handleBlank();
-});
+  console.log(event);
+  ValidateEmail();
+  if (nameInput.value.length === 0) {
+    errorName.textContent = "First name cannot be empty!";
+  } else if (lastName.value.length === 0) {
+    errorLastname.textContent = "Lastname cannot be empty!";
+  } else if (password.value.length === 0) {
+    errorPassword.textContent = "Password cannot be empty!";
+  }
+}
+function ValidateEmail() {
+  const rgx = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+  if (rgx.test(email.value)) {
+    return (errorEmail.textContent = "");
+  } else {
+    errorEmail.textContent = "Looks like this is not an email !";
+    return false;
+  }
+}
+const submitBtn = document.querySelector(".claimBtn");
+submitBtn.addEventListener("click", handleConfirmBtn);
